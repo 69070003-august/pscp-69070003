209fe44211af9ce508d45ded864002f9e36a308d
69070003-august <69070003@kmitl.ac.th>
2026-07-04 23:19:19 +0700

    upload

---
+++ 2988/การตรวจสอบบัตรประชาชน.py
@@ -0,0 +1,10 @@
+"""13"""
+def main():
+    """ooo"""
+    number = len(input())
+    print(number)
+    if number == 13:
+        print("yes")
+    else:
+        print("no")
+main()
---
+++ 2996/[LEARNING LOGS] สลับตัวอักษร.py
@@ -0,0 +1,8 @@
+"""logs"""
+def main():
+    """lll"""
+    name = (input()).lower()
+    for i in range(5,0,-1):
+        print(f"{name[i - 1]}",end ="")
+
+main()
